430dae89fd690a84ef29873b5291dcb5d31904cb
sabrina <sabrinahermans@live.nl>
2023-11-07 13:06:25 +0100

    ik heb de message een class van gemaakt zodat we die aan kunnen roepen met het maken van een message. ben nu bezig met het herschrijven van de write message functie zodat die gebruik maakt van de class

--- index.php
+++ index.php
@@ -1,11 +1,95 @@
 <?php
 ini_set('display_errors', 1);
-// echo "<pre>";
-// var_dump($_SERVER);
-// echo "/pre"
 
 // functie file wegschrijven
 // functie file ophalen
+// functie verwijderen
+
+$messageArr = [];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // echo "<pre>";
+    // var_dump($_POST["name"], $_POST["message"]);
+    // echo "</pre>";
+
+    // $jsonFormat = [$_POST["name"] => $_POST["message"]];
+
+    writeFile($_POST, $messageArr);
+    // writeFile($_POST);
+}
+
+echo "<pre>";
+var_dump(file_get_contents("guests.txt"));
+echo "</pre>";
+
+class Message
+{
+    private $name;
+    private $message;
+
+    public function __construct($name, $message)
+    {
+        $this -> setName($name);
+        $this -> setMessage($message);
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+}
+
+function writeFile($post, $messageArr)
+{
+    // echo "<pre>";
+    // var_dump($post);
+    // echo "</pre>";
+
+    $message = new Message($post["name"], $post["message"]);
+
+    array_push($messageArr, [$message->getName(), $message->getMessage()]);
+
+    echo "<pre>";
+    var_dump($messageArr);
+    echo "</pre>";
+}
+
+// function writeFile($jsonFormat)
+// {
+//     // echo "<pre>";
+//     // var_dump($jsonFormat, $messageArr);
+//     // echo "</pre>";
+
+//     $file = "guests.txt";
+
+//     $currentContents = file_get_contents($file);
+
+//     $prettyJsonString = json_encode($jsonFormat, JSON_PRETTY_PRINT);
+
+//     $currentContents .= $prettyJsonString . ",\n";
+
+//     file_put_contents($file, $currentContents);
+
+//     // echo "<pre>";
+//     // var_dump();
+//     // echo "</pre>";
+
+// }
 ?>
 
 <!DOCTYPE html>
@@ -18,7 +102,7 @@ ini_set('display_errors', 1);
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
     <!-- Use title if it's in the page YAML frontmatter -->
-    <title>Welcome to XAMPP</title>
+    <title>Gastenboek</title>
 
     <meta name="description" content="XAMPP is an easy to install Apache distribution containing MariaDB, PHP and Perl." />
     <meta name="keywords" content="xampp, apache, php, perl, mariadb, open source distribution" />
@@ -42,24 +126,6 @@ ini_set('display_errors', 1);
 
         <button type="submit">Submit</button>
     </form>
-
-    <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $file = "guests.js";
-        $currentContents = file_get_contents($file);
-
-        $prettyJsonString = json_encode($_POST, JSON_PRETTY_PRINT);
-
-        $currentContents .= $prettyJsonString . "\n";
-
-        file_put_contents($file, $currentContents);
-
-        // echo "<pre>";
-        // var_dump($currentContents);
-        // echo "</pre>";
-    }
-    ?>
-
 </body>
 
 
